d695278745642ef3640be16d03a0a5d05ef1b7c5
taurus366 <47596673+taurus366@users.noreply.github.com>
2020-11-21 18:33:26 +0200

    PrintUtil

--- src/main/java/course/springdata/advanced/init/AppInitializer.java
+++ src/main/java/course/springdata/advanced/init/AppInitializer.java
@@ -3,6 +3,7 @@ package course.springdata.advanced.init;
 import course.springdata.advanced.dao.IngredientRepository;
 import course.springdata.advanced.dao.LabelRepository;
 import course.springdata.advanced.dao.ShampooRepository;
+import course.springdata.advanced.util.PrintUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -28,7 +29,7 @@ public class AppInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         shampooRepo.findBySizeOrderById(MEDIUM)
-                .forEach(System.out::println);
+                .forEach(PrintUtil::printShampoo);
 
 
 
